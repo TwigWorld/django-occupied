66a4e8f02228bf323f52b0292ec8b9aa55b42e18
Arun-integrella <arun.jain@integrella.com>
2023-04-28 17:21:56 +0530

    Release version 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-occupied',
-    version='0.1.1',
+    version='2.0.0',
     author='Colin Barnwell',
     description='Adds db-locks to executables.',
     long_description=open('README.md').read(),
